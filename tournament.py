577aa684aa6a0c3669f8a0564d4c63a4f64ca12c
akamuri <akamuri@users.noreply.github.com>
2015-11-02 21:21:49 -0700

    Added cursor setup to Connect()
    This caused the following change in all the methods:
    db, cursor = connect()

--- tournament.py
+++ tournament.py
@@ -5,20 +5,27 @@
 
 import psycopg2
 
-def connect():
-    """Connect to the PostgreSQL database.  Returns a database connection."""
-    return psycopg2.connect("dbname=tournament")
+
+def connect(database_name="tournament"):
+    """Setup the connect method to make connection with the database and also setup 
+    the cursor. An error message is displayed if there is problem with the conection.
+    """
+    try:
+        db = psycopg2.connect("dbname={}".format(database_name))
+        cursor = db.cursor()
+        return db, cursor
+    except:
+        print("Error : Please verify the databse name in the .sql file")
 
 
 def deleteMatches():
     """Remove all the match records from the database.
     Used cursor() to execute SQL command to DELETE all data from the matches table.
     """
-    DB = connect()
-    c = DB.cursor()
-    c.execute("DELETE FROM matches;")
-    DB.commit()
-    DB.close ()
+    db, cursor = connect()
+    cursor.execute("DELETE FROM matches;")
+    db.commit()
+    db.close ()
 
 
 
@@ -26,11 +33,10 @@ def deletePlayers():
     """Remove all the player records from the database.
     Used cursor() to execute SQL command to DELETE all data from the Players table.
     """    
-    DB = connect()
-    c = DB.cursor()
-    c.execute("DELETE FROM Players;")
-    DB.commit()
-    DB.close ()
+    db, cursor = connect()
+    cursor.execute("DELETE FROM Players;")
+    db.commit()
+    db.close ()
 
 
 
@@ -39,29 +45,28 @@ def countPlayers():
     Executed SQL command to COUNT all rows from the Players table.
     Then Used c.fetchone()[0] to obtain the count value and return it.
     """
-    DB = connect()
-    c = DB.cursor()
-    c.execute("SELECT COUNT(*) FROM Players;")
-    countPy = c.fetchone()[0]
-    DB.close ()
+    db, cursor = connect()
+    cursor.execute("SELECT COUNT(*) FROM Players;")
+    countPy = cursor.fetchone()[0]
+    db.close ()
     return countPy
 
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
+
     Args: 
     name: the player's full name (need not be unique).
-    
     Executed INSERT into players table, %s allows the Query to understand 'name'.
     """
-    DB = connect()
-    c = DB.cursor()
-    c.execute("INSERT INTO Players (name) VALUES (%s)",(name,))
-    DB.commit()
-    DB.close()
+    db, cursor = connect()
+    cursor.execute("INSERT INTO Players (name) VALUES (%s)",(name,))
+    db.commit()
+    db.close()
 
 
 def playerStandings():
@@ -77,14 +82,13 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
 
-    Used c.fetchall the results of the SQL Query are converted to a list of tuples and the is returned.
+    Used cursor.fetchall the results of the SQL Query are converted to a list of tuples and the is returned.
     """
-    DB = connect()
-    c = DB.cursor()
-    c.execute("SELECT player_id, name, player_wins, player_matches FROM Players ORDER BY player_wins DESC")
+    db, cursor = connect()
+    cursor.execute("SELECT player_id, name, player_wins, player_matches FROM Players ORDER BY player_wins DESC")
     standings = [(int(row[0]), str(row[1]), int(row[2]),int(row[3])) 
-            for row in c.fetchall()]
-    DB.close ()
+            for row in cursor.fetchall()]
+    db.close ()
     return standings
 
 
@@ -95,18 +99,19 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
 
+
     The SQL Query UPDATE the 'Players' and 'Matches' tables with winner and loser infromation.
     """
-    DB = connect()
-    c = DB.cursor()
-    c.execute("UPDATE Players SET player_wins = player_wins + 1 WHERE player_id = (%s)",(winner,))
-    c.execute("UPDATE Players SET player_matches = player_matches + 1 WHERE player_id = (%s) OR player_id = (%s)",(winner,loser,))
-    c.execute("INSERT INTO Matches VALUES (DEFAULT, (%s) ,(%s))",(winner,loser,))
-    DB.commit()
-    DB.close()
+    db, cursor = connect()
+    cursor.execute("UPDATE Players SET player_wins = player_wins + 1 WHERE player_id = (%s)",(winner,))
+    cursor.execute("UPDATE Players SET player_matches = player_matches + 1 WHERE player_id = (%s) OR player_id = (%s)",(winner,loser,))
+    cursor.execute("INSERT INTO Matches VALUES (DEFAULT, (%s) ,(%s))",(winner,loser,))
+    db.commit()
+    db.close()
 
 
 def swissPairings():    
+
     """Returns a list of pairs of players for the next round of a match.
   
     Assuming that there are an even number of players registered, each player
@@ -124,16 +129,15 @@ def swissPairings():
     Used a for loop the increments by 2 the list of standings can be converted into a list of pairings.
     rowfetch[i]+rowfetch[i+1] allows the for loop to look at 2 playerIDs at the same time and add them to the tuple.
     """
-    DB = connect()
-    c = DB.cursor()
-    c.execute("SELECT player_id, name FROM Players ORDER BY player_wins DESC")
-    rowfetch = c.fetchall()
+    db, cursor = connect()
+    cursor.execute("SELECT player_id, name FROM Players ORDER BY player_wins DESC")
+    rowfetch = cursor.fetchall()
     pairings =[]
     for i in range(0,(len(rowfetch)-1),2):
         if rowfetch[i+1]:
             pairings.append(rowfetch[i]+rowfetch[i+1])
 
-    DB.close ()
+    db.close ()
     return pairings
 
 
